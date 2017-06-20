e0b6628405a3a6415379c1834b06eda8f874f4bc
kientrinhvan <kien.trinhvan@hotmail.com>
2017-06-20 14:47:40 +0700

    First commit

---
+++ demoFunctionalUITesting/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demoFunctionalUITesting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Double x = 0, y = 0, result = 0;
+            if (Double.TryParse(textBox1.Text, out x) && Double.TryParse(textBox2.Text, out y))
+            {
+                // you know that the parsing attempt
+                // was successful
+                result = x + y;
+                label2.Text = result.ToString();
+            }
+            else
+            {
+                label2.Text = "Error";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
