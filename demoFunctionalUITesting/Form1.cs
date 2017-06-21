24757f77e5b711b52978d4bafe840f77ac293f0f
kientrinhvan <kien.trinhvan@hotmail.com>
2017-06-21 14:44:20 +0700

    New commit

--- demoFunctionalUITesting/Form1.cs
+++ demoFunctionalUITesting/Form1.cs
@@ -38,5 +38,10 @@ namespace demoFunctionalUITesting
         {
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
